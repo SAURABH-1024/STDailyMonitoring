75ed6e77d51d7530dbf0d4ac449a8cb6222a8d62
saurabh ambesange <saam@camelot-itlab.com>
2024-05-27 14:52:52 +0530

    change: formatting changes in date file

--- Dates.js
+++ Dates.js
@@ -2,23 +2,24 @@
 const system = [
     {
         hostname: "test.stmicro-avatar.camelot-itlab.com",
-        triggeredAt: { "between": ["2024-05-23T13:40:00.000Z", "2024-05-24T10:40:00.000Z"] },
+        triggeredAt: { "between": ["2024-05-24T13:40:00.000Z", "2024-05-25T10:40:00.000Z"] },
         instance: "TEST"
     },
     {
         hostname: "stmicro-avatar.camelot-itlab.com",
-        triggeredAt: { "between": ["2024-05-23T13:40:00.000Z", "2024-05-24T10:40:00.000Z"] },
+        triggeredAt: { "between": ["2024-05-24T13:40:00.000Z", "2024-05-25T10:40:00.000Z"] },
+
         instance: "PROD"
     },
     {
         hostname: "dev.stmicro-avatar.camelot-itlab.com",
-        triggeredAt: { "between": ["2024-05-23T13:40:00.000Z", "2024-05-24T10:40:00.000Z"] },
+        triggeredAt: { "between": ["2024-05-24T13:40:00.000Z", "2024-05-25T10:40:00.000Z"] },
+
         instance: "DEV"
     },
     {
         hostname: "pre-prod.stmicro-avatar.camelot-itlab.com",
-        // triggeredAt: { "gt": " 2024-05-23T13:40:00.000Z " },
-        triggeredAt: { "between": ["2024-05-23T13:40:00.000Z", "2024-05-24T10:40:00.000Z"] },
+        triggeredAt: { "between": ["2024-05-24T13:40:00.000Z", "2024-05-25T10:40:00.000Z"] },
         instance: "PRE-PROD"
     }]
 
